5f9296fd74b9232dbb21622759a7288bb61034c6
barreirabruno <brunobarreirasilva@gmail.com>
2021-08-21 16:15:28 -0300

    refactor: should return true on success

--- src/test/java/data/usecases/DbAddExpenseTest.java
+++ src/test/java/data/usecases/DbAddExpenseTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.when;
 
 class SystemUnderTest {
@@ -42,9 +43,10 @@ public class DbAddExpenseTest {
     public void should_return_true_on_success() {
         Expense newExpense = new ExpenseMock().mockExpense();
         SystemUnderTest systemUnderTest = new SystemUnderTest();
-        when(systemUnderTest.getAddExpenseRepositorySpy().add(newExpense)).thenReturn(1);
-        DbAddExpense dbAddExpense = systemUnderTest.makeSut();
-        assertEquals(true, dbAddExpense.add(newExpense));
+        AddExpenseRepositorySpy mock = systemUnderTest.getAddExpenseRepositorySpy();
+        Mockito.when(mock.add(newExpense)).thenReturn(1);
+        assertTrue(systemUnderTest.makeSut().add(newExpense));
+        Mockito.verify(mock).add(newExpense);
     }
 
     @Test
